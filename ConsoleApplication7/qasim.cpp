8b341b1b545bf9c0b1b6ce06a71565ebb1e86171
qasim <bscs24113@itu.edu.pk>
2025-06-09 11:32:50 -0700

    "Add qasim.h and qasim.cpp for utility functions"

---
+++ ConsoleApplication7/qasim.cpp
@@ -0,0 +1,4 @@
+#include"qasim.h"
+using namespace std;
+#include<iostream>
+//cout << "hello world";
---
+++ ConsoleApplication7/qasim.h
@@ -0,0 +1,6 @@
+#ifndef QAS_Hh
+#define QAS_Hh
+#include<iostream>
+using namespace std;
+//pakistan
+#endif
